8541164a9fcd7f26397a15675c27efb978c4e0e8
igor.ribeiro <igorapalvesribeiro@gmail.com>
2024-12-17 18:59:25 -0300

    feat: created method to delete pet

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -10,3 +10,9 @@ def test_list_pets():
     response = repo.list_pets()
     print()
     print(response)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_delete_pet():
+    name = "belinha"
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
